2a120796a135338177fca2b265b30bf9554817ee
Cristian Ramirez <cristian@wishpond.com>
2024-01-03 08:21:26 -0600

    Add image resources for landing page and finish styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,23 +8,23 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
-    // TODO: change names
-    colors: {
-      'main-color': '#2c3639',
-      'white': '#ffffff',
-      'black': '#000000',
-      'signIn': '#9ff1e5',
-      'signUp': '#ecffc8',
-      'fontColor': '#0c5d75',
-      'footerGray': '#f5f5f5',
-      'editIcon': '#047487',
-      'removeIcon': '#920404',
-      'evaluationStrong': '#126f05',
-      'evaluationLight': '#ddffd9',
-      'chapterStrong': '#05636f',
-      'surveyStrong': '#686f05',
-    },
+    // TODO: change namesk
     extend: {
+      colors: {
+        'main-color': '#2c3639',
+        'white': '#ffffff',
+        'black': '#000000',
+        'signIn': '#9ff1e5',
+        'signUp': '#ecffc8',
+        'fontColor': '#0c5d75',
+        'footerGray': '#f5f5f5',
+        'editIcon': '#047487',
+        'removeIcon': '#920404',
+        'evaluationStrong': '#126f05',
+        'evaluationLight': '#ddffd9',
+        'chapterStrong': '#05636f',
+        'surveyStrong': '#686f05',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
