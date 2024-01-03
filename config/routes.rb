6cc1605bd8a4c940d3b0805e727d0b37841bd006
Cristian Ramirez <cristian@wishpond.com>
2024-01-03 09:26:13 -0600

    Add devise gem, add sign up and sign in tailwind css styling and devise configurations for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  devise_for :users
   get 'pages/index'
   root 'pages#landing_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
