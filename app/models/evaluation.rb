08e8ecccd5f24caed524e82ff5eb9b917f2ae5ef
Cristian Ramirez <cristian@wishpond.com>
2024-01-04 19:28:11 -0600

    feat: Add default scope to evaluation

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,6 +1,8 @@
 class Evaluation < Section
   after_initialize :set_default_kind, if: :new_record?
 
+  default_scope { where(kind: :evaluation) }
+
 private
 
   def set_default_kind
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
 
-  has_many :evaluations, -> { where(kind: :evaluation) }
+  has_many :evaluations
 end
